12da7fb84b8005d62fbbb2a49c116698cc5a8094
n-pretorious <pretoriousndungu@gmail.com>
2020-10-14 06:49:29 +0300

    include gitignore

---
+++ news/views.py
@@ -0,0 +1,90 @@
+from types import new_class
+from news.models import Article, NewsLetterRecipients
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import redirect, render
+from .forms import NewsLetterForm
+
+import datetime as dt
+
+# Create your views here.
+def welcome(request):
+    return render(request, "welcome.html")
+
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
+
+def news_today(request):
+    date = dt.date.today()
+    news = Article.todays_news()
+    
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+            HttpResponseRedirect('news_today')
+    else:
+        form = NewsLetterForm()    
+    return render(request, "all-news/today-news.html", {"date": date, "news": news})
+
+
+def past_days_news(request, past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, "%Y-%m-%d").date()
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_today)
+
+    news = Article.days_news(date)
+    return render(request, "all-news/past-news.html", {"date": date, "news": news})
+
+
+def search_results(request):
+
+    if "article" in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Article.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(
+            request,
+            "all-news/search.html",
+            {"message": message, "articles": searched_articles},
+        )
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, "all-news/search.html", {"message": message})
+
+
+def article(request, article_id):
+    try:
+        article = Article.objects.get(id=article_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request, "all-news/article.html", {"article": article})
